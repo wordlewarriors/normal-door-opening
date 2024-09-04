9a82a23ee8b2132f416d4fa8961b9a899a7990b3
Jamie Smith <jamie.smith@cloudreach.com>
2024-09-04 14:13:27 +0100

    Quartz sync: Sep 4, 2024, 2:13 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🎲",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-GB",
+    baseUrl: "wordlewarriors.github.io/normal-door-opening/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
